4cd8c4f1359ff19a507574e284b3416e6c589fe5
harshita1470 <60317695+harshita1470@users.noreply.github.com>
2020-01-29 23:26:11 +0530

    changed

--- app/src/main/java/com/example/ecomm/Login.java
+++ app/src/main/java/com/example/ecomm/Login.java
@@ -4,13 +4,43 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+    Button merchant,customer;
+    TextView newUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        merchant=findViewById(R.id.merchant);
+        customer=findViewById(R.id.customer);
+        newUser=findViewById(R.id.newUser);
+        merchant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(Login.this,merchantlogin.class);
+                startActivity(i);
 
+            }
+        });
+        customer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(Login.this,customerlogin.class);
+                startActivity(i);
+
+            }
+        });
+          newUser.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View v) {
+
+              }
+          });
     }
 }
---
+++ app/src/main/java/com/example/ecomm/customerlogin.java
@@ -0,0 +1,14 @@
+package com.example.ecomm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class customerlogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customerlogin);
+    }
+}
